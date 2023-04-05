6fa8694d5f6d17aa296d73fe9cbec2f15a04bab6
Paulo Sergio <ngcpaulo1@gmail.com>
2023-04-04 22:16:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// 10
+let question = Number(prompt('Digite o M da area do quadrado\n'));
+
+function areaQuad() {
+    return (question * question) / 2;
+}
+console.log('A área do quadrado é', areaQuad(), 'e o dobro dele é', areaQuad() ** 2);
+
+
